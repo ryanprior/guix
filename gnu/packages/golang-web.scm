5e071df9db9e5774b8f18cf926f1abb0a301c7a9
Ryan Prior <rprior@protonmail.com>
2021-01-17 23:48:32 -0600

    gnu: Add go-github-com-puerkitobio-urlesc.
    * gnu/local.mk: New file golang-web.scm.
    * gnu/packages/golang-web.scm (go-github-com-puerkitobio-urlesc): New variable.

---
+++ gnu/packages/golang-web.scm
@@ -0,0 +1,35 @@
+;;; Copyright © 2021 Ryan Prior <rprior@protonmail.com>
+
+(define-module (gnu packages golang-web)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages golang)
+  #:use-module (guix build-system go)
+  #:use-module (guix git-download)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix packages))
+
+(define-public go-github-com-puerkitobio-urlesc
+  (package
+    (name "go-github-com-puerkitobio-urlesc")
+    (version "de5bf2ad457846296e2031421a34e2568e304e35")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/PuerkitoBio/urlesc")
+             (commit version)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32
+         "0n0srpqwbaan1wrhh2b7ysz543pjs1xw2rghvqyffg9l0g8kzgcw"))))
+    (build-system go-build-system)
+    (arguments
+     '(#:import-path "github.com/PuerkitoBio/urlesc"))
+    (home-page "https://github.com/PuerkitoBio/urlesc")
+    (synopsis "Implements query escaping as per RFC 3986")
+    (description
+     "This package escapes queries as per RFC 3986.  It contains some parts of
+the @code{net/url} package, modified so as to allow some reserved characters
+incorrectly escaped by net/url.")
+    (license license:expat)))
+
