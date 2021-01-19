d44bfb5479f829823cec942d04c02e3df83c55f8
Ryan Prior <rprior@protonmail.com>
2021-01-19 00:41:26 -0600

    gnu: Add go-github-com-armon-go-radix.
    * gnu/local.mk: New file golang-datastructures.scm.
    * gnu/packages/golang-datastructures.scm (go-github-com-armon-go-radix): New variable.

---
+++ gnu/packages/golang-datastructures.scm
@@ -0,0 +1,34 @@
+;;; Copyright © 2021 Ryan Prior <rprior@protonmail.com>
+
+(define-module (gnu packages golang-datastructures)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages golang)
+  #:use-module (guix build-system go)
+  #:use-module (guix git-download)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix packages))
+
+(define-public go-github-com-armon-go-radix
+  (package
+    (name "go-github-com-armon-go-radix")
+    (version "1.0.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/armon/go-radix")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32
+         "1m1k0jz9gjfrk4m7hjm7p03qmviamfgxwm2ghakqxw3hdds8v503"))))
+    (build-system go-build-system)
+    (arguments
+     '(#:import-path "github.com/armon/go-radix"))
+    (home-page "https://github.com/armon/go-radix")
+    (synopsis "Radix tree data structure library for golang.")
+    (description
+     "This package provides a radix tree implementation optimized for sparse
+nodes.")
+    (license license:expat)))
+
